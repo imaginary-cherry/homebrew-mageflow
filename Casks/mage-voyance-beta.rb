2597fdf1500cf9ca448ab6b4b4470ff7288fa779
YedidyaHKfir <Yedidya@dono.ai>
2026-03-23 01:35:42 +0200

    Add initial cask formulas for mage-voyance stable and beta

---
+++ Casks/mage-voyance-beta.rb
@@ -0,0 +1,10 @@
+cask "mage-voyance-beta" do
+  version "0.0.0"
+  sha256 "PLACEHOLDER"
+
+  url "https://github.com/imaginary-cherry/mageflow/releases/download/app/v#{version}-beta/Mageflow.Viewer_#{version}_aarch64.dmg"
+  name "Mage Voyance Beta"
+  homepage "https://github.com/imaginary-cherry/mageflow"
+
+  app "Mage Voyance.app"
+end
