a047940901b70f53570834b880749050af2006cf
YedidyaHKfir <Yedidya@dono.ai>
2026-04-06 11:41:33 +0300

    cask: update mage-voyance stable to 0.1.0

--- Casks/mage-voyance.rb
+++ Casks/mage-voyance.rb
@@ -1,6 +1,6 @@
 cask "mage-voyance" do
   version "0.1.0"
-  sha256 "540d3a5328cdf57bf7c59de791fb553fabdafdf19cbc47fc1283d7420d87fa02"
+  sha256 "1ff0ef41afeba677da22e249b63c87e7e973e0c7d290fc6d8f208a094050de26"
 
   url "https://github.com/imaginary-cherry/mageflow/releases/download/app/v#{version}/Mage.Voyance_#{version}_aarch64.dmg"
   name "Mage Voyance"
